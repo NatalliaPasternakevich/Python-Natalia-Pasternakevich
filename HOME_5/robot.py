61cd7164cb87124c51320403e5cce6471949d8b8
pasternak <pasternakevichnatalia@gmail.com>
2022-09-20 22:22:02 +0300

    проверка

--- HOME_5/robot.py
+++ HOME_5/robot.py
@@ -1,9 +1,15 @@
 import math
+x = 0
+y = 0
 command = [0, 0]
 while True:
     command = input("\nВвод команды ").split()
     if command == (" ").split():
         break
+y = y + command[1]
+y = y - command[1]
+x = x + command[1]
+x = x - command[1]
 
 range = math.sqrt((x ** 2) + (y ** 2))
 
